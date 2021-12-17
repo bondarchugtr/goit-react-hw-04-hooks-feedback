52ff2cb33c32b9c874a2475b38fd178387f98c44
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-12-17 22:34:02 +0200

    +

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -1,56 +1,59 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import s from "./Feedback.module.css";
 import FeedbackOptions from "../Button/FeedbackOptions";
 import Statistics from "../Statistics/Statistics";
 import Notification from "../Notification/Notification";
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onSetVoice = (state) => {
+    switch (state) {
+      case "good":
+        setGood((state) => state + 1);
+        break;
+      case "neutral":
+        setNeutral((state) => state + 1);
+        break;
+      case "bad":
+        setBad((state) => state + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  onSetVoice = (type) => {
-    this.setState((prevState) => ({ [type]: prevState[type] + 1 }));
-  };
+  const countTotalFeedback = good + neutral + bad;
 
-  countTotalFeedback = () => {
-    const objKey = Object.keys(this.state);
-    return objKey.reduce((total, el) => (total += this.state[el]), 0);
-  };
+  const countPositiveFeedbackPercentage = Math.round(
+    (good / countTotalFeedback) * 100
+  );
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
+  const keys = ["good", "neutral", "bad"];
 
-  render() {
-    const total = this.countTotalFeedback();
-    const positive = this.countPositiveFeedbackPercentage();
-    const keys = Object.keys(this.state);
+  return (
+    <div>
+      <h1 className={s.title}>Please leave feedback</h1>
+      <FeedbackOptions options={keys} onLeaveFeedback={onSetVoice} />
 
-    return (
-      <div>
-        <h1 className={s.title}>Please leave feedback</h1>
-        <FeedbackOptions options={keys} onLeaveFeedback={this.onSetVoice} />
-
-        <div className={s.statistics}>
-          <h2 className={s.title__statistics}>Statistics</h2>
-          {total > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-              positive={positive}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </div>
+      <div className={s.statistics}>
+        <h2 className={s.title__statistics}>Statistics</h2>
+        {countTotalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback}
+            positive={countPositiveFeedbackPercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Feedback;
